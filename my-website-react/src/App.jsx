53e0cc91a4b7c0813f4d79d705f16e1f396a0107
Thomas Liu <tliu3157@gmail.com>
2026-04-07 11:30:45 -0700

    small fix

--- my-website-react/src/App.jsx
+++ my-website-react/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react"
-import Nav from "src/components/Nav"
-import Header from "src/components/Header"
-import ProjectCard from "src/components/ProjectCard"
+import Nav from "src/components/Nav.jsx"
+import Header from "src/components/Header.jsx"
+import ProjectCard from "src/components/ProjectCard.jsx"
 
 function useFadeIn() {
   const ref = useRef(null)
